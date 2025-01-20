71f69f2e6fe033500f3d945b14706c50aca3c69a
Joseph Reid <joseph.james.reid@gmail.com>
2025-01-20 14:34:30 -0800

    working drag and drop, delete

--- app/api/coach_availability/route.ts
+++ app/api/coach_availability/route.ts
@@ -1,23 +1,91 @@
-import { PrismaClient } from '@prisma/client';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-  const coachId = url.searchParams.get('coachId'); 
+  const coachId = url.searchParams.get("coachId");
 
   try {
-    const whereCondition = coachId
-    ? { coachId: Number(coachId) } 
-    : {}; 
+    const whereCondition = coachId ? { coachId: Number(coachId) } : {};
 
-    const coachAvailabilities = await prisma.coachAvailability.findMany({where: whereCondition});
-    console.log(coachAvailabilities)
+    const coachAvailabilities = await prisma.coachAvailability.findMany({
+      where: whereCondition,
+    });
+    console.log(coachAvailabilities);
     return NextResponse.json(coachAvailabilities); // Return data as JSON
   } catch (error) {
-    console.error('Error fetching coach availability:', error);
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+    console.error("Error fetching coach availability:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request: Request) {
+  const { startTime, EndTime, coachId } = await request.json();
+
+  try {
+    const newAvailability = await prisma.coachAvailability.create({
+      data: {
+        startTime: new Date(startTime),
+        EndTime: new Date(EndTime),
+        coachId: Number(coachId),
+      },
+    });
+
+    return NextResponse.json(newAvailability, { status: 201 });
+  } catch (error) {
+    console.error("Error creating coach availability:", error);
+    return NextResponse.json(
+      { error: "Failed to create availability" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  const { id, startTime, EndTime, coachId } = await request.json();
+
+  try {
+    const updatedAvailability = await prisma.coachAvailability.update({
+      where: {
+        id: Number(id), // Specify the unique identifier (e.g., id)
+      },
+      data: {
+        startTime: new Date(startTime),
+        EndTime: new Date(EndTime),
+        coachId: Number(coachId),
+      },
+    });
+
+    return NextResponse.json(updatedAvailability, { status: 201 });
+  } catch (error) {
+    console.error("Error creating coach availability:", error);
+    return NextResponse.json(
+      { error: "Failed to create availability" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+
+  try {
+    const deletedAvailability = await prisma.coachAvailability.delete({
+      where: { id: Number(id) },
+    });
+
+    return NextResponse.json(deletedAvailability, { status: 200 });
+  } catch (error) {
+    console.error("Error deleting coach availability:", error);
+    return NextResponse.json(
+      { error: "Failed to delete availability" },
+      { status: 500 }
+    );
   }
 }
--- app/components/coach_availability_calendar.tsx
+++ app/components/coach_availability_calendar.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import { Scheduler } from "@aldabil/react-scheduler";
-import { ProcessedEvent } from "@aldabil/react-scheduler/types";
+import { ProcessedEvent, EventActions } from "@aldabil/react-scheduler/types";
 import { WeekProps } from "@aldabil/react-scheduler/views/Week";
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 type CoachAvailability = {
   id: number;
   startTime: Date;
-  EndTime: Date; //Typo in migration here :/ 
+  EndTime: Date; //TODO: Fix typo in migration here :/
   createdAt: Date;
   updatedAt: Date;
   coachId: number;
@@ -20,65 +20,200 @@ interface CoachAvailabilityProps {
   coachName: String;
 }
 
-
-export const CoachAvailability: React.FC<CoachAvailabilityProps> = ({ userId, coachName }) => {
-  
-  const [coachAvailabilities, setCoachAvailabilities] = useState<CoachAvailability[]>([]);
+export const CoachAvailability: React.FC<CoachAvailabilityProps> = ({
+  userId,
+  coachName,
+}) => {
+  const [coachAvailabilities, setCoachAvailabilities] = useState<
+    CoachAvailability[]
+  >([]);
 
   useEffect(() => {
     const fetchCoachAvailability = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/api/coach_availability', {
-          params: {
-            coachId: userId,
-          },
-        });
+        const response = await axios.get(
+          "http://localhost:3000/api/coach_availability",
+          {
+            params: {
+              coachId: userId,
+            },
+          }
+        );
         setCoachAvailabilities(response.data);
       } catch (error) {
-        console.error('Error fetching coach availability:', error);
+        console.error("Error fetching coach availability:", error);
       }
     };
 
     fetchCoachAvailability();
-  }, [userId]); 
-    
+  }, [userId]);
+
+  const handleEventConfirm = async (
+    event: ProcessedEvent,
+    action: EventActions
+  ) => {
+    if (action === "create") {
+      try {
+        const startTime = new Date(event.start);
+        const endTime = new Date(startTime);
+        endTime.setHours(startTime.getHours() + 2); //This is a work around until we can make this work through the picker logic.
+
+        event.end = endTime;
+
+        const newAvailability = {
+          startTime: startTime,
+          EndTime: endTime,
+          coachId: userId,
+        };
+
+        console.log("Sending body:", newAvailability);
+
+        const response = await axios.put(
+          "http://localhost:3000/api/coach_availability",
+          newAvailability
+        );
+        setCoachAvailabilities((prevState) =>
+          prevState.map((avail) =>
+            avail.id === event.event_id ? response.data : avail
+          )
+        );
+      } catch (error) {
+        console.error("Error adding new availability:", error);
+      }
+    }
+    return event;
+  };
+
+  const handleEventDrop = async (
+    event: React.DragEvent<HTMLButtonElement>, // Drag event from the button
+    droppedOn: Date,
+    updatedEvent: ProcessedEvent,
+    originalEvent: ProcessedEvent
+  ) => {
+    console.log("here");
+    console.log(event);
+    console.log(droppedOn);
+    console.log(originalEvent);
+    console.log(updatedEvent);
+    try {
+      const startTime = new Date(updatedEvent.start);
+      const endTime = new Date(updatedEvent.end);
+
+      const updatedAvailability = {
+        id: updatedEvent.event_id,
+        startTime: startTime,
+        EndTime: endTime,
+        coachId: userId,
+      };
+
+      console.log(updatedAvailability);
+
+      // Send PUT request to update the availability on the server
+      const response = await axios.post(
+        `http://localhost:3000/api/coach_availability`,
+        updatedAvailability
+      );
+
+      // Update local state with the updated availability
+      setCoachAvailabilities((prevState) =>
+        prevState.map((avail) =>
+          avail.id === updatedEvent.event_id ? response.data : avail
+        )
+      );
+    } catch (error) {
+      console.error("Error updating availability:", error);
+    }
+  };
+  //   try {
+  //     const startTime = new Date(updatedEvent.start);
+  //     const endTime = new Date(startTime);
+  //     endTime.setHours(startTime.getHours() + 2); //This is a work around until we can make this work through the picker logic.
+
+  //     const newAvailability = {
+  //       startTime: startTime,
+  //       EndTime: endTime,
+  //       coachId: userId,
+  //     };
+
+  //     const response = await axios.post('http://localhost:3000/api/coach_availability', newAvailability);
+  //     setCoachAvailabilities(prevState => prevState.map(avail =>
+  //       avail.id === updatedEvent.event_id ? response.data : avail
+  //     ));
+  //   } catch (error) {
+  //     console.error('Error adding new availability:', error);
+  //   }
+  // };
+
+  // const handleEventEdit = async (updatedEvent: ProcessedEvent) => {
+  //   try {
+  //     const startTime = new Date(updatedEvent.start);
+  //     const endTime = new Date(startTime);
+  //     endTime.setHours(startTime.getHours() + 2);
+
+  //     const updatedAvailability = {
+  //       startTime: startTime,
+  //       EndTime: endTime,
+  //       coachId: userId,
+  //     };
+
+  //     // Send PUT request to update the availability on the server
+  //     const response = await axios.put(
+  //       `http://localhost:3000/api/coach_availability/${updatedEvent.event_id}`,
+  //       updatedAvailability
+  //     );
+
+  //     // Update local state with the updated availability
+  //     setCoachAvailabilities((prevState) =>
+  //       prevState.map((avail) =>
+  //         avail.id === updatedEvent.event_id ? response.data : avail
+  //       )
+  //     );
+  //   } catch (error) {
+  //     console.error("Error updating availability:", error);
+  //   }
+  // };
+
+  const handleEventDelete = async (deletedEventId: Number | String) => {
+    try {
+      await axios.delete(
+        `http://localhost:3000/api/coach_availability?id=${deletedEventId}`
+      );
+
+      setCoachAvailabilities((prevState) =>
+        prevState.filter((avail) => avail.id !== deletedEventId)
+      );
+    } catch (error) {
+      console.error("Error deleting availability:", error);
+    }
+  };
+
   const events: ProcessedEvent[] = coachAvailabilities.map((availability) => ({
     event_id: availability.id, // Unique identifier
     title: `Coach ${coachName} Availability`,
-    start: new Date(availability.startTime), 
+    start: new Date(availability.startTime),
     end: new Date(availability.EndTime),
-    allDay: false, 
+    allDay: false,
   }));
 
-  console.log(events)
-
-  const weekSettings: WeekProps = { 
-    weekDays: [0, 1, 2, 3, 4, 5], 
-    weekStartOn: 0, 
-    startHour: 9, 
+  const weekSettings: WeekProps = {
+    weekDays: [0, 1, 2, 3, 4, 5],
+    weekStartOn: 0,
+    startHour: 9,
     endHour: 17,
-    step: 30,
+    step: 60,
     navigation: false,
-    disableGoToDay: true
+    disableGoToDay: true,
   };
 
-  return (  
-    <Scheduler 
+  return (
+    <Scheduler
       view="week"
+      disableViewNavigator={true}
       week={weekSettings}
-      events={events} // Pass transformed events
-    //   onEventAdd={(event) => {
-    //     console.log("Event Added:", event);
-    //     // Handle event addition logic here (e.g., API call)
-    //   }}
-    //   onEventUpdate={(event) => {
-    //     console.log("Event Updated:", event);
-    //     // Handle event update logic here
-    //   }}
-    //   onEventDelete={(eventId) => {
-    //     console.log("Event Deleted:", eventId);
-    //     // Handle event deletion logic here
-    //   }}
+      events={events}
+      onEventDrop={handleEventDrop}
+      onConfirm={handleEventConfirm}
+      onDelete={handleEventDelete}
     />
   );
 };
